152669012506378e7388c8e852856b1af36f2861
Tiran-work <gulian2014@rambler.ru>
2024-03-15 13:29:05 +0300

    dasdas

--- js/module7-task1.js
+++ js/module7-task1.js
@@ -3,12 +3,13 @@ import { CREAT_PHOTO } from './data.js';
 const list = document.querySelector('.pictures');
 function photo(array) {
   const blockElementLink = document.querySelector('#picture').content.querySelector('.picture');
-  const imgTeg = blockElementLink.querySelector('.picture__img');
-  const commentTeg = blockElementLink.querySelector('.picture__comments');
-  const likes = blockElementLink.querySelector('.picture__likes');
+
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < array.length; i++) {
     const cloneBlockElementLink = blockElementLink.cloneNode(true);
+    const imgTeg = cloneBlockElementLink.querySelector('.picture__img');
+    const commentTeg = cloneBlockElementLink.querySelector('.picture__comments');
+    const likes = cloneBlockElementLink.querySelector('.picture__likes');
     imgTeg.src = CREAT_PHOTO[i].url;
     imgTeg.alt = CREAT_PHOTO[i].description;
     commentTeg.textContent = CREAT_PHOTO[i].comments.length;
